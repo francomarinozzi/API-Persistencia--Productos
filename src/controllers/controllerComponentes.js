9a5b3cdddaca06d32e93c2a68828270dd1b1eab7
francomarinozzi <francomarinozzi4@gmail.com>
2024-10-16 16:55:04 -0300

    Update componentes

--- src/controllers/controllerComponentes.js
+++ src/controllers/controllerComponentes.js
@@ -1,8 +1,17 @@
 
+const {Componente, Fabricante, Producto} = require('../models/')
 
-const getComponentes = (req,res) =>{
 
+const getComponentes = async(req,res) =>{
+    try{
+        const componentes = await Componente.findAll()
+        return res.status(200).json(componentes)
+    }
+    catch{
+        return res.status(404)
+    }
 }
+
 
 module.exports = {
     getComponentes
